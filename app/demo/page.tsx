5fca34501ce582ef0e6fe98410d87af44c472bbd
İnanç Akduvan <49209002+inancakduvan@users.noreply.github.com>
2025-05-18 00:53:39 +0300

    prerender testing

--- app/demo/page.tsx
+++
@@ -1,8 +0,0 @@
-import Products from "@/components/products";
-import { fetchProducts } from "@/requests";
-
-export default async function Home() {
-  const products = await fetchProducts();
-
-  return <Products products={products} />;
-}
